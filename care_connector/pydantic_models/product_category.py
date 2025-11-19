0b931f76f6a791af9f5d4817fa1217f9ea876b0c
visakhdc <visakhv2182@gmail.com>
2025-11-19 15:43:40 +0530

    [FIX]:Product category remove archive option

--- care_connector/pydantic_models/product_category.py
+++ care_connector/pydantic_models/product_category.py
@@ -1,13 +1,6 @@
 from pydantic import BaseModel
-from enum import Enum
-
-class CategoryStatus(Enum):
-    active = 'active'
-    retired = 'retired'
-    draft = 'draft'
 
 class CategoryData(BaseModel):
     category_name: str
     x_care_id: str
     parent_x_care_id: str | None = None
-    status: CategoryStatus | None = None
