31ad1408d4ca984dd673662ec0674148b4e79c33
visakhdc <visakhv2182@gmail.com>
2025-11-23 13:08:46 +0530

    [IMP]: Reconciliation report

--- ssmm_bank_reconciliation/__manifest__.py
+++ ssmm_bank_reconciliation/__manifest__.py
@@ -9,5 +9,6 @@
         "security/ir.model.access.csv",
         "views/bank_reconciliantion_views.xml",
         "views/account_move_views.xml",
+        "wizard/bank_reconciliation_report_wizard_views.xml",
     ],
 }
