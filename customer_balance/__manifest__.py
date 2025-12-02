788680d48a1e036dc037dd220fecab187c66fa68
visakhdc <visakhv2182@gmail.com>
2025-12-02 15:46:16 +0530

    [ADD]:customer_balance

---
+++ customer_balance/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "customer_balance",
+
+    'summary': "Shows The Customer Balance in Customer Portal",
+
+    'description': """
+    """,
+
+    'category': 'Contacts',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'views/inherit_res_partner_views.xml',
+    ],
+}
+
