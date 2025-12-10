e40c5f89e449aed2a5d15638005a289a92817ef9
visakhdc <visakhv2182@gmail.com>
2025-12-10 16:42:08 +0530

    [FIX]: Add license number in vendor bill

--- grn_number_in_vendor_bill/models/res_partner.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-class ResPartner(models.Model):
-    """Inheriting res.partner"""
-    _inherit = "res.partner"
-
-    drug_license_number = fields.Char(string='Drug License Number',store=True)
