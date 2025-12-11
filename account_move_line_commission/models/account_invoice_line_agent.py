52f944e888f3bb916da3081e653805ad97535ead
visakhdc <visakhv2182@gmail.com>
2025-12-11 20:02:33 +0530

    [FIX]:commission line status fix

--- account_move_line_commission/models/account_invoice_line_agent.py
+++ account_move_line_commission/models/account_invoice_line_agent.py
@@ -13,29 +13,25 @@ class AccountInvoiceLineAgent(models.Model):
     def _compute_commission_state(self):
         for rec in self:
             invoice = rec.invoice_id
-
             if not invoice:
                 rec.commission_state = 'draft'
                 continue
+
+            payment_ids = invoice.matched_payment_ids
+            if not payment_ids:
+                rec.commission_state = 'draft'
+                continue
+
+            any_paid = any(payment.state == 'paid' for payment in payment_ids)
+            all_cancelled = all(payment.state == 'canceled' for payment in payment_ids)
+            if any_paid and invoice.state == 'posted':
+                rec.commission_state = 'posted'
 
             if invoice.state == 'cancel':
                 rec.commission_state = 'cancelled'
                 continue
 
-            payments = invoice.matched_payment_ids
-
-            if not payments:
-                rec.commission_state = 'draft'
-                continue
-
-            any_paid = any(p.state == 'paid' for p in payments)
-
-            all_cancelled = all(p.state == 'canceled' for p in payments)
-
-            if any_paid:
-                rec.commission_state = 'posted'
-            elif all_cancelled:
+            if all_cancelled:
                 rec.commission_state = 'cancelled'
-            else:
-                rec.commission_state = 'draft'
+
 
