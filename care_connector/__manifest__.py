d3947af3efc78731d287463cf8d755110d12e82d
Bodhish Thomas <bodhish@gmail.com>
2026-01-03 11:09:47 +0530

    Clean up odoo imports

--- care_connector/__manifest__.py
+++ care_connector/__manifest__.py
@@ -14,7 +14,6 @@
         "views/account_payment_views.xml",
         "views/product_template_views.xml",
         "views/res_partner_views.xml",
-        "views/res_user_views.xml",
         "views/product_category_views.xml",
         "views/bill_counter_views.xml",
     ],
--- care_connector/models/__init__.py
+++ care_connector/models/__init__.py
@@ -1,2 +1,2 @@
 from . import product_template, account_move, res_partner, account_payment
-from . import product_category, bill_counter, res_user
+from . import product_category, bill_counter
