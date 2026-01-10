07446f63115dcb6427910b4f25783862c644df4b
Bodhish Thomas <bodhish@gmail.com>
2026-01-10 20:19:16 +0530

    Clean up unused modules

--- invoice_reports/wizard/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import hsn_invoice_report
-from . import sales_invoice_report
-from . import purchase_bill_report
-from . import general_ledger_report
-from . import trial_balance_report
-from . import partner_aging_report
