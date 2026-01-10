ca765d7e4e48d31d3c733536038b3d3d24559060
Bodhish Thomas <bodhish@gmail.com>
2026-01-10 19:53:36 +0530

    Clean up accounting customisation

---
+++ sales_journal_tax_selection/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': "Sales Journal Tax Auto-Selection",
+    'summary': "Auto-select sales journal based on invoice tax presence",
+    'description': """
+Automatically selects the appropriate sales journal based on whether 
+the invoice contains taxes:
+
+- Mark a journal as "Taxed Journal" for invoices with taxes
+- Mark a journal as "Untaxed Journal" for invoices without taxes
+
+Compatible with Odoo Enterprise.
+    """,
+    'category': 'Accounting/Accounting',
+    'version': '18.0.1.0.0',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/account_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
